1ac31f05932e1c0fcbd7626c8a7f9f8b8cf7e247
Danny Boyer <dannyboyer49@gmail.com>
2020-12-09 20:09:26 -0500

    Day9 2/2 star

--- src/main/kotlin/day9/EncodingError.kt
+++ src/main/kotlin/day9/EncodingError.kt
@@ -17,9 +17,27 @@ object EncodingError {
         var from = 0
         var to = preambleSize
 
-        for (n in numberList.slice(preambleSize..numberList.size - 1)) {
+        for (n in numberList.slice(preambleSize until numberList.size)) {
             if (!isValid(n, numberList.subList(from++, to++))) {
                 return n
+            }
+        }
+        return -1
+    }
+
+    fun isWeakness(num: Long, numberList: List<Long>): Boolean {
+        return num == numberList.sum()
+    }
+
+    fun findWeakness(num: Long, numberList: List<Long>): Long {
+        for (n in 2..100) {
+            var from = 0
+            var to = n
+            while (to < numberList.size) {
+                val subList = numberList.subList(from++, to++)
+                if (isWeakness(num, subList)) {
+                    return subList.minOrNull()!! + subList.maxOrNull()!!
+                }
             }
         }
         return -1
@@ -32,8 +50,8 @@ fun main() {
     openStream.bufferedReader().useLines { lines -> lines.forEach { numbers.add(it.toLong()) } }
 
     val answer1 = EncodingError.findInvalidNum(numbers, 25)
-    //val answer2 = ReportRepair.findThreeEntries(reportExpenses)
+    val answer2 = EncodingError.findWeakness(answer1, numbers)
 
     println("First part: $answer1")
-    //rintln("Second part: $answer2")
+    println("Second part: $answer2")
 }
--- src/test/kotlin/day9/EncodingErrorTest.kt
+++ src/test/kotlin/day9/EncodingErrorTest.kt
@@ -57,4 +57,36 @@ internal class EncodingErrorTest {
         )
         assertEquals(127, invalidNum)
     }
+
+    @Test
+    internal fun isWeakness() {
+        assertTrue(EncodingError.isWeakness(127, listOf(15, 25, 47, 40)))
+    }
+
+    @Test
+    internal fun findWeakness() {
+        val numberList = listOf(
+            35,
+            20,
+            15,
+            25,
+            47,
+            40,
+            62,
+            55,
+            65,
+            95,
+            102,
+            117,
+            150,
+            182,
+            127,
+            219,
+            299,
+            277,
+            309,
+            576
+        ).map { it.toLong() }
+        assertEquals(62, EncodingError.findWeakness(127, numberList))
+    }
 }
